4688b6201d7c57d2aed5c6643f8324bd2ff52865
Joel Lee <lee.joel395@gmail.com>
2019-01-17 14:50:31 -0800

    Basic Character Movement, Animation Controller setup

---
+++ RobotCharacterProject/Assets/_Scripts/Character.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Character : MonoBehaviour
+{
+    private Movement _movementComponent;
+
+    private void Start() {
+        _movementComponent = GetComponent<Movement>();
+    }
+
+    private void Update() {
+        //side to side movement
+        _movementComponent.Move(Input.GetAxis("Horizontal"));
+
+        //jumping, don't really need to get the vertical axis as this is impulse
+        if ((Input.GetKeyDown(KeyCode.UpArrow) || (Input.GetKeyDown(KeyCode.W))) && _movementComponent.IsGrounded()) {
+            _movementComponent.Jump();
+        }
+        //punching aka interacting
+        if(Input.GetKeyDown(KeyCode.Space) && _movementComponent.IsGrounded()) {
+            Punch();
+        }
+        //check front for a punchable object
+        //raycast
+    }
+
+    private void Punch() {
+        //punch an object
+    }
+}
---
+++ RobotCharacterProject/Assets/_Scripts/Movement.cs
@@ -0,0 +1,76 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+/// <summary>
+/// Accessed by Character to handle movement.
+/// </summary>
+public class Movement : MonoBehaviour
+{
+    private CharacterController _controller;
+    [SerializeField]
+    private float _speed, _jumpHeight;
+
+    private float _centerToGround;
+    private bool _canMove, _inAir;
+    private Vector3 _movement;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        _controller = GetComponent<CharacterController>();
+        _centerToGround = GetComponent<Collider>().bounds.extents.y; //get the height in the y direction
+        if (!_controller) {
+            gameObject.AddComponent<CharacterController>();
+        }
+    }
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        _movement.x = 0; //remove any front to back movement
+        //_movement = transform.TransformDirection(_movement);
+        _controller.Move(_movement * Time.deltaTime);
+
+        _inAir = IsGrounded();
+        if (!_inAir) {
+            _movement += Physics.gravity * Time.deltaTime;
+        }
+        //removed the built up gravitational force when landing and replace with a small downforce
+        else if (_inAir) {
+            _movement.y = -_controller.stepOffset / Time.deltaTime;
+        }
+    }
+
+    /// <summary>
+    /// Adds vertical movement.
+    /// </summary>
+    public void Jump() {
+        _movement.y = 0f;
+        _movement.y += _jumpHeight;
+    }
+
+    /// <summary>
+    /// Move the character by movement speed in direction
+    /// </summary>
+    public void Move(float direction) {
+        _movement.z = direction * _speed;
+    }
+
+    private RaycastHit _hitInfo;
+    /// <summary>
+    /// Check if the character is on the ground
+    /// </summary>
+    /// <returns></returns>
+    public bool IsGrounded() {
+        /**
+        Physics.Raycast(transform.position, Vector3.down, out _hitInfo, _centerToGround + .01f);
+        if (_hitInfo.transform) {
+            return _hitInfo.transform.CompareTag("Ground");
+        }
+        else {
+            return false;
+        }**/
+        return _controller.isGrounded;
+    }
+}
